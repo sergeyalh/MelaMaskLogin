6953e37901098e1b075e99c40fe2ea95c4f177bd
sergeyalh <sergeyalh1@gmail.com>
2023-04-26 18:29:20 +0300

    Add files via upload

---
+++ MetaMaskConnection/src/WalletCard.js
@@ -0,0 +1,91 @@
+import React, {useState} from 'react'
+import {ethers} from 'ethers'
+import axios from 'axios';
+import './WalletCard.css'
+
+const WalletCard = () => {
+
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+	const [UserNft, setUserNft] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+	const endpoint = "https://api-rinkeby.etherscan.io/api";
+	const apikey = "7NMKRJY2ZR3HAS4C2TJWJT4727S459RR6G";
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+			console.log('MetaMask Here!');
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+				getAccountBalance(result[0]);
+				getAccountBalancNft(result[0]);
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			
+			});
+
+		} else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+	// update account, will cause component re-render
+	const accountChangedHandler = (newAccount) => {
+		setDefaultAccount(newAccount);
+		getAccountBalance(newAccount.toString());
+		getAccountBalancNft(newAccount);
+	}
+
+	const getAccountBalance = (account) => {
+		window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
+		.then(balance => {
+			setUserBalance(ethers.utils.formatEther(balance));
+		})
+		.catch(error => {
+			setErrorMessage(error.message);
+		});
+	};
+
+	const getAccountBalancNft = (account) => {
+		axios.get((endpoint + '?module=account&action=tokennfttx&contractaddress='+ account + '&page=1&offset=100&tag=latest&apikey=' + apikey)).then(outputb => {
+			const { result } = outputb.data
+            setUserNft(result);
+        })
+		.catch(error => {
+			setErrorMessage(error.message);
+		});
+	};
+
+	const chainChangedHandler = () => {
+		// reload the page to avoid any errors with chain change mid use of application
+		window.location.reload();
+	}
+
+
+	// listen for account changes
+	window.ethereum.on('accountsChanged', accountChangedHandler);
+
+	window.ethereum.on('chainChanged', chainChangedHandler);
+	
+	return (
+		<div className='walletCard'>
+		<h4> {"Connection to MetaMask using window.ethereum methods"} </h4>
+			<button onClick={connectWalletHandler}>{connButtonText}</button>
+			<div className='accountDisplay'>
+				<h3>Address: {defaultAccount}</h3>
+			</div>
+			<div className='balanceDisplay'>
+				<h3>Balance: {userBalance}</h3>
+			</div>
+			{errorMessage}
+		</div>
+	);
+}
+
+export default WalletCard;
